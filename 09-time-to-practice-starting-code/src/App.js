211d59c3abfb7abf2d7cdabc4703e55024cc93de
ibalejandro <asanch41@eafit.edu.co>
2023-04-09 23:44:35 -0500

    Add LoadingSpinner in Suspense component to wrap definition of all Routes.

--- 09-time-to-practice-starting-code/src/App.js
+++ 09-time-to-practice-starting-code/src/App.js
@@ -1,21 +1,30 @@
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
 import NotFound from "./pages/NotFound";
 import QuoteDetail from "./pages/QuoteDetail";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
 
 function App() {
   return (
     <div>
       <Layout>
-      <Routes>
-        <Route path={"/"} element={<Navigate to="quotes" />}/>
-        <Route path={"quotes"} element={<AllQuotes />} />
-        <Route path={"quotes/:quoteId/*"} element={<QuoteDetail />} />
-        <Route path={"new-quote"} element={<NewQuote />} />
-        <Route path={"*"} element={<NotFound />}></Route>
-      </Routes>
+      <Suspense 
+        fallback={
+          <div className="centered"><LoadingSpinner /></div>
+        }
+      >
+        <Routes>
+          <Route path={"/"} element={<Navigate to="quotes" />}/>
+          <Route path={"quotes"} element={<AllQuotes />} />
+          <Route path={"quotes/:quoteId/*"} element={<QuoteDetail />} />
+          <Route path={"new-quote"} element={<NewQuote />} />
+          <Route path={"*"} element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
       </Layout>
     </div>
   );
